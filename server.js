f64e65d198df0f89411dc766660c9d856e3cc94a
shimonaeman <shimonaeman@gmail.com>
2017-06-29 12:55:42 +0300

    initial up for web starter with node.js

---
+++ server.js
@@ -0,0 +1,45 @@
+const express= require('express');
+const hbs= require('hbs');
+const fs= require('fs');
+
+var app=express();
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+app.use(express.static(__dirname+'/public'));
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+app.use((req,res,next)=>{
+  var now =new Date().toString();
+  var log = `${now} : ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log',log+'\n',(err)=>{
+    if(err)
+    console.log('unable to append');
+  });
+  next();
+});
+
+app.get('/bad',(req,res)=>{
+  res.send({
+    'error massege':'cant get response'
+  }
+);
+});
+
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    home: 'Home page',
+    header:'Home Page',
+
+  });
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    header: 'About Page'
+  });
+});
+
+app.listen(3000);
